18a7933c1a25659748aa2ffd9d8ef8bd891a37dc
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-07-09 19:40:55 +0300

    Fixing bugs

--- crud-redux-router-server/server.js
+++ crud-redux-router-server/server.js
@@ -1,35 +1,35 @@
-const jsonServer = require('json-server')
-const clone = require('clone')
-const data = require('./db.json')
+const jsonServer = require('json-server');
+const clone = require('clone');
+const data = require('./db.json');
 const cors = require('cors');
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-server.use(cors({
-    origin: 'http://localhost:3000',
+server.use(
+  cors({
+    origin: true, // Allow requests from any origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }))
+  })
+);
 
 // For mocking the POST request, POST request won't make any changes to the DB in production environment
 const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
-    _isFake: isProductionEnv
-})
-const middlewares = jsonServer.defaults()
+  _isFake: isProductionEnv,
+});
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
 
 server.use((req, res, next) => {
-    if (req.path !== '/')
-        router.db.setState(clone(data))
-    next()
-})
+  if (req.path !== '/') router.db.setState(clone(data));
+  next();
+});
 
-server.use(router)
+server.use(router);
 server.listen(process.env.PORT || 8000, () => {
-    console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
