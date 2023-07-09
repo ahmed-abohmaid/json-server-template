b2e0d99f478e7b927cdc2299b12a8226283c2304
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-07-09 18:22:46 +0300

    Adding cors to fix POST bug

--- crud-redux-router-server/server.js
+++ crud-redux-router-server/server.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server')
 const clone = require('clone')
 const data = require('./db.json')
+const cors = require('cors');
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
+server.use(cors());
 
 // For mocking the POST request, POST request won't make any changes to the DB in production environment
 const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
